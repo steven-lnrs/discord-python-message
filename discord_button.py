c58ee0d97898ccba7bab12f724acda2ad93c50e9
unusually_calm <72924838+unusually-calm@users.noreply.github.com>
2023-01-12 13:32:42 -0500

    Update discord_button.py

--- discord_button.py
+++ discord_button.py
@@ -1,2 +1,6 @@
+#this repo is still in development/the creator is kinda lazy
 import requests
+#Please place your webhook here
 webhookurl = ''
+
+message = input("what would you like to send to the discord?\n")
