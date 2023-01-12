8c31cfc6f960319216d2dfccbc4f6b09179120c2
unusually_calm <72924838+unusually-calm@users.noreply.github.com>
2023-01-12 16:39:29 -0500

    Update discord_button.py

--- discord_button.py
+++ discord_button.py
@@ -4,3 +4,18 @@ import requests
 webhookurl = ''
 
 message = input("what would you like to send to the discord?\n")
+#how to send it
+data = {
+    "content" : message,
+
+    "username" : "python test"
+}
+
+result = requests.post(webhookurl, json = data)
+#check for error (shouldn't be any)
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("sent".format(result.status_code))
