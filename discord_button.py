c044308a2aa185a8bf8b93f7ad76f99d7bf618c3
unusually_calm <72924838+unusually-calm@users.noreply.github.com>
2023-02-05 14:58:35 -0500

    Update discord_button.py

--- discord_button.py
+++ discord_button.py
@@ -1,8 +1,31 @@
 #this repo is still in development/the creator is kinda lazy
-#made by unusually_calm#8486
+print('made by unusually_calm#8486')
 import requests
 #Please place your webhook here
 webhookurl = ''
+bot_name = input("what do you want to name the bot?\n")
+
+message = input("what would you like to send to the discord?\n")
+#how to send it
+data = {
+    "content" : message,
+
+    "username" : bot_name
+}
+
+result = requests.post(webhookurl, json = data)
+#check for error (shouldn't be any unless the discord webhook is invalid)
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("sent".format(result.status_code))
+#this repo is still in development/the creator is kinda lazy
+#made by unusually_calm#8486
+import requests
+#Please place your webhook here
+webhookurl = input('what webhook would you like to use?\n')
 bot_name = input("what do you want to name the bot?\n")
 
 message = input("what would you like to send to the discord?\n")
