a73d1d684834c2d995a71a62c70eb34d644b8794
unusually_calm <72924838+unusually-calm@users.noreply.github.com>
2023-02-18 15:46:26 -0500

    Update discord_button.py

--- discord_button.py
+++ discord_button.py
@@ -6,8 +6,11 @@ os.system('cls')
 print("made by unusually_calm")
 time.sleep(3)
 webhookurl = input('what webhook would you like to use?\n')
+time.sleep(1)
 bot_name = input("what do you want to name the bot?\n")
+time.sleep(1)
 message = input("what would you like to send to the discord?\n")
+time.sleep(1)
 #how to send it
 data = {
     "content" : message,
